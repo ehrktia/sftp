b3fd71a6e4c852adc823a99eeda0ea526bdba493
ehrktia <1096406+ehrktia@users.noreply.github.com>
2019-06-19 19:24:22 +0100

    test coverage added to helpers

--- pwdfilgen/pwdfilgen_test.go
+++ pwdfilgen/pwdfilgen_test.go
@@ -1,5 +1,32 @@
 package pwdfilgen
-import "testing"
 
-func TestGenPwdFile(t *testing T){
+import (
+	"encoding/hex"
+	"io/ioutil"
+	"log"
+	"os"
+	"testing"
+)
+
+func TestGenPwdFile(t *testing.T) {
+	//check file
+	Filnam := "pwd"
+	inptxt := []byte("pwd")
+	GenPwdFile(inptxt)
+	_, err := os.Stat(Filnam)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//check if inp txt is encoded
+	encodedout := hex.EncodeToString(inptxt)
+	content, err := ioutil.ReadFile(Filnam)
+	if err != nil {
+		log.Fatal(err)
+	}
+	actualfilcontent := []byte(content)
+	actual := string(actualfilcontent)
+	if encodedout != actual {
+		t.Errorf("expected: %s received: %s", encodedout, actual)
+	}
+
 }
