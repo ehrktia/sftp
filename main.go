7ecefa8e8772d489af276786ce8497384caba542
ehrktia <1096406+ehrktia@users.noreply.github.com>
2019-09-13 21:33:00 +0100

    adding helpcontent out file and test for main

--- main.go
+++ main.go
@@ -6,21 +6,19 @@ import (
 	"sftp/helpcontent"
 	inp "sftp/input"
 	pwdfilgen "sftp/pwdfilgen"
-	"sftp/sshClient"
+
+	"github.com/sftp/sshClient"
 )
 
-/* main operations */
-func main() {
-	inpval := os.Args[1:]
-
-	if len(inpval) < 1 {
+func operation(ip []string) {
+	if len(ip) < 1 {
 		log.Print(helpcontent.Helpcontent())
 	} else {
-		switch string(inpval[0]) {
+		switch string(ip[0]) {
 		case "help":
 			log.Print(helpcontent.Helpcontent())
 		case "GenPwdFile":
-			genpwd := []byte(inpval[1])
+			genpwd := []byte(ip[1])
 			log.Print(genpwd)
 			if len(string(genpwd)) < 7 {
 				log.Fatal("ERROR: min length for pwd is 7 characters")
@@ -28,11 +26,11 @@ func main() {
 			pwdfilgen.GenPwdFile(genpwd)
 
 		case "conn":
-			usr := []byte(inpval[1])
+			usr := []byte(ip[1])
 			pwd := pwdfilgen.DecodePwd()
-			url := []byte(inpval[2])
-			srcdir := []byte(inpval[3])
-			tgtdir := []byte(inpval[4])
+			url := []byte(ip[2])
+			srcdir := []byte(ip[3])
+			tgtdir := []byte(ip[4])
 			inp.SetUserInp(string(usr), string(pwd), string(url), string(srcdir), string(tgtdir))
 			log.Printf("TRACE:\t starting conn for %s", inp.Inp.Uname)
 			log.Printf("TRACE:\t Password acquired for user  %s from %s", string(usr), pwdfilgen.Filnam)
@@ -41,12 +39,13 @@ func main() {
 			log.Printf("TRACE:\t starting conn for %s", inp.Inp.Url)
 			log.Printf("TRACE:\t starting conn for %s", inp.Inp.SrcDir)
 			log.Printf("TRACE:\t starting conn for %s", inp.Inp.TgtDir)
-			// sshFilOperation.sshFilOpr() file operation in the new func
-
 		default:
 			log.Print(helpcontent.Helpcontent())
 		}
-
 	}
+}
 
+/* main operations */
+func main() {
+	operation(os.Args[1:])
 }
---
+++ main_test.go
@@ -0,0 +1,15 @@
+package main
+
+import "testing"
+
+func TestOperation(t *testing.T) {
+	t.Run("check inp values",
+		func(t *testing.T) {
+			data := []string{"help"}
+			if data[0]="help" {
+				helpcontent.Helpcontent()
+				
+			}
+		})
+
+}
