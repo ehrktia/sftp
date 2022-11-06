46864db30e079b926bdbd799567aa398af43b292
karthick <a.karthie+github@gmail.com>
2022-11-06 17:43:01 +0000

    :ambulance: fix unit test initial fix

--- sshClient/sshClient_test.go
+++ sshClient/sshClient_test.go
@@ -1,30 +1,15 @@
 package sshClient
 
 import (
-	"sftp/pwdfilgen"
+	"log"
 	"testing"
-
-	"golang.org/x/crypto/ssh"
 )
 
-var inp = []struct {
-	inp      string
-	pwd      string
-	protocol string
-	host     string
-	exp      ssh.Session
-}{
-	{"test", "tester", "tcp", "localhost:22", ssh.Session},
-	{"test1", "tester1", "tcp", "localhost:22", ssh.Session},
-	{"test2", "tester2", "tcp", "localhost:22", ssh.Session},
-}
-
 func TestSSHSession(t *testing.T) {
-	//setstring up uname,pwd as inp val
-	for _, v := range inp {
-		uname := v.inp
-		pwd := pwdfilgen.DecodePwd(v.pwd)
-		exp := v.exp
+	l := log.Default()
+	_, err := SSHSession(l, t.Name(), t.Name(), "localhost:22")
+	if err == nil {
+		t.Fatal("expected to fail,got no error")
 	}
 
 }
